694e1c78f541fc0cfe86d0cb09360baadf65702d
Asante.dev <davidameyaw607@gmail.com>
2025-05-25 20:56:04 +0000

    Add category schema: create a new document type for categories with fields for title and description, enhancing content organization and management capabilities.

---
+++ sanity/schemaTypes/index.js
@@ -0,0 +1,6 @@
+import blockContent from './blockContent'
+import category from './category'
+import academicEvent from './academicEvent'
+import author from './author'
+
+export const schemaTypes = [academicEvent, author, category, blockContent]
