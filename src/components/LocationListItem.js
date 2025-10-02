46322fd91ccf9240640972e42a5dac6948818929
Asante.dev <davidameyaw607@gmail.com>
2025-10-02 22:03:27 +0000

    Add LocationListItem component: create a new reusable component for displaying location details with an icon, title, and description, including responsive styling for improved user interaction.

---
+++ src/components/LocationListItem.js
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const LocationListItem = ({ iconName, title, description, onPress, style }) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={onPress}
+      style={[styles.wrapper, style]}
+    >
+      <ImageBackground
+        source={require("../../assets/images/card1.png")}
+        resizeMode="cover"
+        style={styles.bg}
+        imageStyle={styles.bgImage}
+      >
+        <View style={styles.leftSection}>
+          <Ionicons
+            name={iconName}
+            size={24}
+            color="#9B0E10"
+            style={styles.leftIcon}
+          />
+          <View style={styles.textBlock}>
+            <Text style={styles.title}>{title}</Text>
+            <View style={styles.descriptionBox}>
+              {description ? (
+                <Text numberOfLines={2} style={styles.descriptionText}>
+                  {description}
+                </Text>
+              ) : null}
+            </View>
+          </View>
+        </View>
+        <Ionicons name="arrow-forward" size={20} color="#9B0E10" />
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    alignSelf: "center",
+    width: "92%",
+    borderRadius: 12,
+    overflow: "hidden",
+  },
+  bg: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 25,
+    paddingHorizontal: 16,
+    backgroundColor: "#dedede",
+  },
+  bgImage: {
+    borderRadius: 12,
+  },
+  leftSection: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+  },
+  leftIcon: {
+    marginRight: 10,
+  },
+  textBlock: {
+    flex: 1,
+  },
+    title: {
+    fontSize: 16,
+    color: "#121212",
+    fontWeight: "700",
+    letterSpacing: 0.2,
+  },
+  descriptionBox: {
+    marginTop: 8,
+    minHeight: 28,
+    // backgroundColor: "#ffffff",
+    borderRadius: 6,
+    width: "75%",
+    justifyContent: "center",
+    paddingHorizontal: 0,
+  },
+  descriptionText: {
+    color: "#555555",
+    fontSize: 12,
+    marginLeft: 0,
+    lineHeight: 18,
+  },
+});
+
+export default LocationListItem;
