b09b165dc33a7075e2ac93a94fd6d7f961b17163
Asante.dev <davidameyaw607@gmail.com>
2025-10-04 21:55:54 +0000

    Add locations to schemaTypes: include locations schema in the export for enhanced data structure and integration with location-related components.

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import academicEvent from './academicEvent'
 import author from './author'
+import locations from './locations'
 
-export const schemaTypes = [academicEvent, author, category, blockContent]
+export const schemaTypes = [academicEvent, author, category, blockContent, locations]
