a982a45b127fad372c1aab68014310ee71e11301
Asante.dev <davidameyaw607@gmail.com>
2025-10-07 21:10:21 +0000

    Add locations data: create a new locationsData.js file containing detailed information about various campus locations, including categories, descriptions, images, and operating hours for enhanced data accessibility.

---
+++ data/locationsData.js
@@ -0,0 +1,65 @@
+import category from "../sanity/schemaTypes/category";
+
+export const locations = [
+  {
+    category: "school_libraries",
+    name: "Prempeh II Library, KNUST",
+    description:
+      "The Prempeh II Library is the main library of the Kwame Nkrumah University of Science and Technology. It is a modern library with a capacity of 1,000 students.",
+    image: "../assets/locations/prempeh_library.png",
+    hours: "Mon–Sat, 9AM–10PM",
+    latitude: 6.67547,
+    longitude: -1.57171,
+  },
+  {
+    category: "school_libraries",
+    name: "KNUST Library Mall",
+    description:
+      "The KNUST Library Mall is a modern library with a capacity of 1,000 students.",
+    image: "../assets/locations/library_mall.png",
+    hours: "Mon–Fri, 8:30 AM – 10 PM",
+    latitude: 6.67751,
+    longitude: -1.56476,
+  },
+  {
+    category: "colleges",
+    name: "College of Engineering",
+    description:
+      "The KNUST Library Mall is a modern library with a capacity of 1,000 students.",
+    image: "../assets/locations/coe.png",
+    hours: "Mon–Fri, 8:00 AM – 5 PM",
+    latitude: 6.6733,
+    longitude: -1.5649,
+  },
+  {
+    category: "colleges",
+    name: "College of Agriculture and Natural Resources",
+    description:
+      "The KNUST Library Mall is a modern library with a capacity of 1,000 students.",
+    image: "../assets/locations/agric.png",
+    hours: "Mon–Sat, 8:00 AM – 5 PM",
+    latitude: 6.67756,
+    longitude: -1.56424,
+  },
+  {
+    category: "colleges",
+    name: " College of Art and Built Environment (CABE)",
+    description:
+      "The KNUST Library Mall is a modern library with a capacity of 1,000 students.",
+    image: "../assets/locations/coe.png",
+    hours: "Mon–Sat, 8:30 AM – 10 PM",
+    latitude: 6.6733,
+    longitude: -1.5649,
+  },
+  {
+    category: "colleges",
+    name: "College of Engineering",
+    description:
+      "The KNUST Library Mall is a modern library with a capacity of 1,000 students.",
+    image: "../assets/locations/coe.png",
+    hours: "Mon–Sat, 8:30 AM – 10 PM",
+    latitude: 6.6733,
+    longitude: -1.5649,
+  },
+];
+
