0688abdd3a02703391bb82660733c74033503d03
Asante.dev <davidameyaw607@gmail.com>
2025-10-18 20:32:50 +0000

    Add guides to schemaTypes: include the guides schema in the export for enhanced content management and organization.

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -3,5 +3,6 @@ import category from './category'
 import academicEvent from './academicEvent'
 import author from './author'
 import locations from './locations'
+import guides from './guides'
 
-export const schemaTypes = [academicEvent, author, category, blockContent, locations]
+export const schemaTypes = [academicEvent, author, category, blockContent, locations, guides]
